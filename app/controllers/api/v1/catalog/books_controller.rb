898642ac41de8e15556adb0d8722bb39bc0a9ffa
FilipTurniski <32130197+filipturniski@users.noreply.github.com>
2022-08-09 18:40:54 +0200

    edit show books
    now it can be seen number of available books. added postman test

--- app/controllers/api/v1/catalog/books_controller.rb
+++ app/controllers/api/v1/catalog/books_controller.rb
@@ -2,6 +2,13 @@ module Api
   module V1
     module Catalog
       class BooksController < ApplicationController
+
+        def index
+          books = BookView.order('name ASC')
+          render json: {status: 'SUCCESS', message: 'Loaded Books', data:books}, status: :ok
+        end
+
+
         def search
           books = BookView.select('name').order('name ASC')
           books = books.search(params[:bookName]) if params[:bookName].present?
